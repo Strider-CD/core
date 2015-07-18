63f5e4923f1bdcaf05798fb1a3e3c7e385a64c27
Ilya Radchenko <ilya@burstcreations.com>
2015-07-18 09:59:00 -0400

    Model for drone, v1 api base

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 var Hapi = require('hapi')
 var Primus = require('primus')
-// var Joi = require('joi')
+var Joi = require('joi')
 var config = require('config')
+var Drone = require('./models/drone')
 var server = new Hapi.Server()
+var basePath = '/api/v' + config.apiVersion + '/'
 
 server.connection({
   port: config.port
@@ -19,22 +21,9 @@ primus.on('connection', function (spark) {
   })
 })
 
-// Testing onPostAuth for protobuf transformation
-// we can remove this and hadle that after the fact.
-server.ext('onPostAuth', function (request, reply) {
-  var payload = request.payload
-
-  if (payload) {
-    payload.onPostAuth = true
-    reply(payload)
-  }
-
-  reply.continue()
-})
-
 server.route([
   {
-    path: '/api/drones',
+    path: basePath + 'drones',
     method: 'GET',
     handler (request, reply) {
       reply([
@@ -42,15 +31,28 @@ server.route([
       ])
     }
   }, {
-    path: '/api/drones/{id}/checkin',
+    path: basePath + 'drones',
+    method: 'POST',
+    config: {
+      validate: {
+        payload: {
+          name: Joi.string().required()
+        }
+      }
+    },
+    handler (request, reply) {
+      Drone.create(request.payload)
+        .then(reply)
+    }
+  }, {
+    path: basePath + 'drones/{id}/checkin',
     method: 'PUT',
     handler (request, reply) {
       var id = request.params.id
       var payload = request.payload
 
-      payload.droneId = id
-
-      reply(payload)
+      Drone.checkIn(id, payload)
+        .then(reply)
     }
   }
 ])
---
+++ models/drone.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var Promise = require('bluebird')
+var mockDrones = []
+
+exports.create = function (data) {
+  data.id = 1
+  data.token = 'mocktoken.1'
+
+  mockDrones.push(data)
+
+  return Promise.resolve(data)
+}
+
+exports.checkIn = function (id, data) {
+  return Promise.resolve('ok')
+}
