06950d62d0785f66813920ddc8306718789d4f4a
Ilya Radchenko <ilya@burstcreations.com>
2015-08-26 18:38:25 -0400

    Add projects list w/ tests

---
+++ test/project.js
@@ -0,0 +1,30 @@
+'use strict'
+// post on testing hapi https://medium.com/the-spumko-suite/testing-hapi-services-with-lab-96ac463c490a
+
+require('babel/register')
+
+var tape = require('tape')
+var server = require('../index')
+var config = require('config')
+var Project = require('../lib/models/project').Project
+
+var apiPrefix = config.apiPrefix
+
+tape('projects - list', function (t) {
+  // clean drone collection
+  Project.collection().purge()
+
+  var options = {
+    url: apiPrefix + 'projects',
+    method: 'GET'
+  }
+
+  server.inject(options, function (res) {
+    var data = res.result
+
+    t.equal(res.statusCode, 200)
+    t.ok(data && Array.isArray(data), 'Data is array')
+    t.ok(data.length === 0, 'Data has no results')
+    t.end()
+  })
+})
