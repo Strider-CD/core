f70f153d354b067a2ef8b8d62166946778069b42
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2015-08-27 15:09:34 +0200

    matrix jobs + receive new jobs via rest API

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var logger = require('./lib/log.js')(module)
 var apiRoutes = require('./lib/routes/api')
 var eventHandlers = require('./lib/eventHandlers')
 var server = new Hapi.Server()
+var Rooms = require('primus-rooms')
 
 server.connection({
   port: config.port
@@ -17,10 +18,38 @@ var emitter = new EventEmitter()
 eventHandlers(emitter)
 
 var primus = Primus(server.listener, {})
+primus.use('rooms', Rooms)
 primus.on('connection', function (spark) {
   spark.write('ping')
   spark.on('data', function (data) {
-    logger.info(data)
+    if (data.room) {
+      if (data.save) {
+        // save the buffered data into the database
+        return
+      }
+      var room = data.room
+      var message = data.msg
+
+      // check if spark is already in this room
+      if (~spark.rooms().indexOf(room)) {
+        if (message && message.lineNumber && message.line) {
+          console.log(message.lineNumber + ': ' + message.line)
+          spark.room(room).write(message)
+        }
+      } else {
+        if (data.action == 'leave') {
+          spark.leave(room)
+          return
+        }
+        // join the room
+        spark.join(room, function () {
+          if (message && message.lineNumber && message.line) {
+            console.log(message.lineNumber + ': ' + message.line)
+            spark.room(room).write(message)
+          }
+        })
+      }
+    }
   })
 })
 
